fa32ac175f557e17ed11f214b68a76159b9e3da0
ishant9805 <ishant9805@gmail.com>
2023-12-18 00:47:16 +0530

    completed my password generator project

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,33 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [length, setLength] = useState(8)
+  const [numeric, setNumeric] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [pass, setPass] = useState('')
+
+  const passRef = useRef(null)
+
+  const passGen = useCallback(() => {
+    let password = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numeric) str += "0123456789"
+    if (charAllowed) str += "~`!@#$%^&*()_-+={}[]:|<>,.?/"
+    for (let index = 1; index <= length; index++) {
+      password += str.charAt(Math.floor(Math.random() * str.length + 1))
+    }
+    console.log(password)
+    setPass(password)
+  }, [length, numeric, charAllowed, setPass])
+
+  const copyToClip = useCallback(() => {
+    passRef.current?.select()
+    passRef.current?.setSelectionRange(0,32)
+    window.navigator.clipboard.writeText(pass)
+  }, [pass])
+
+  useEffect(() => {
+    passGen()
+  }, [length, charAllowed, numeric, passGen])
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='w-full max-w-2xl mx-auto shadow-md rounded-3xl p-4 my-8 text-orange-300 bg-gray-700'>
+        <h1 className='text-5xl text-center my-6 text-slate-400'>Password Generator</h1>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+          <input
+            type="text" 
+            value={pass}
+            className='outline-none rounded-md w-full py-2 px-4'
+            placeholder='Password'
+            readOnly
+            ref={passRef}
+          />
+          <button 
+            className='
+              outline-none 
+              bg-blue-400 
+              px-4 py-1 shrink-0 
+              text-gray-800'
+            onClick={() => {copyToClip()}}>
+            Copy
+          </button>
+        </div>
+        <div className='flex text-lg gap-x-2'>
+          <div className="flex items-center gap-x-1">
+            <input 
+              type="range"
+              min={4}
+              max={32}
+              value={length}
+              className='ml-2 cursor-e-resize w-36'
+              id='passLength'
+              onChange={(e) => {setLength(e.target.value)}}
+            />
+            <label htmlFor="passLength">Length({length})</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input 
+              type="checkbox"
+              defaultChecked={numeric}
+              id="numInput"
+              onChange={() => {
+                setNumeric((prev) => !prev);
+              }}
+              className='w-4 h-4 cursor-pointer'
+            />
+            <label htmlFor="numInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input 
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="charInput"
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+              className='w-4 h-4 cursor-pointer'
+            />
+            <label htmlFor="charInput">Special Characters</label>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
